f29bd4a7651b6191329682933bf577a6cc6eca96
lijiahao <lijiahao@shopee.com>
2022-07-27 19:22:58 +0800

    curl

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Logger, Param, ParseIntPipe, Post, Query, UseFilters, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './filter/httpExceptionFilter';
+import { RolesGuard } from './guard/authGuard';
 
+// @UseGuards(RolesGuard)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // @UseFilters(HttpExceptionFilter)
+  @Get('api/cat')
+  @UseGuards(RolesGuard)
+  getCat(@Query('number', new ParseIntPipe({errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE})) number: number, @Query('type') type: string): string {
+    // throw new HttpException({
+    //   status: 403,
+    //   error: 'haha'
+    // }, HttpStatus.FORBIDDEN)
+
+    Logger.error('[p0.1] params', { number, type })
+    return JSON.stringify({
+      number: 5,
+      type: '金渐层'
+    })
+  }
+
+  @UseGuards(RolesGuard)
+  @Post('api/updateCat')
+  postCat(@Body() body): string {
+    Logger.error('[p0.1] body', body)
+    return JSON.stringify({
+      code: 200,
+      msg: '成功'
+    })
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './controller/cats.controller';
+import { RolesGuard } from './guard/authGuard';
+import { LoggerMiddleware } from './middleWare/loggerMiddleware';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard
+    }
+  ],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('api/cat')
+  }
+}
