3fa5412a74e341108058562b4cbb7c22249f4146
Ethan199412 <lijiahao1213ok@163.com>
2023-06-07 22:26:47 +0800

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,6 @@ import { LoggerMiddleware } from './middleware/loggerMiddleware';
 
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('cats');
+    consumer.apply(LoggerMiddleware).forRoutes('cats'); 
   }
 }
