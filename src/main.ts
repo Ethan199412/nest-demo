36cf35890554de046a64a04edca63d332a801222
lijiahao <lijiahao@shopee.com>
2023-06-08 19:40:15 +0800

    pipe

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './filter/httpExceptionFilter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalFilters(new HttpExceptionFilter())
+  app.enableCors()
   await app.listen(3010);
 
 }
