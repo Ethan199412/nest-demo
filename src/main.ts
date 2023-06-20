a685e82c061c9bef6df4f5b1f3a39d6497579edc
lijiahao <lijiahao@shopee.com>
2023-06-20 21:11:35 +0800

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { HttpExceptionFilter } from './filter/httpExceptionFilter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new HttpExceptionFilter())
   app.enableCors()
+
+  // app.useGlobalFilters(new HttpExceptionFilter())
+  const options = new DocumentBuilder().setTitle('xiaoT的飞机').setDescription('xxx').setVersion('1').build()
+  const doc = SwaggerModule.createDocument(app, options)
   await app.listen(3010);
 
 }
